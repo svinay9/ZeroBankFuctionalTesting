b1c2b341cbc4ecb8f13730a591d75f9444f8bd69
Your Name <you@example.com>
2020-12-18 17:52:09 -0500

    again changing the password to true agian with changes in add payye as well

--- src/test/java/testcases/TC003_AddpayeeVerify.java
+++ src/test/java/testcases/TC003_AddpayeeVerify.java
@@ -23,12 +23,14 @@ public class TC003_AddpayeeVerify extends MainClass {
 	lp = new LoginPage(driver);
 	//hp = new HomePage(driver);
 	ap = new AddpayeePage(driver);
-	
+	String expected = "Zero - Log in";
 	test.log(LogStatus.INFO, "add Payee Test started");
 	app_logs.info("add Payee test started");
 	//hp.clicksigninbtn();
 	test.log(LogStatus.INFO, "Entered Username and Password");
+	if (driver.getTitle().equalsIgnoreCase(expected)){
 	lp.dologin("username", "password");
+	}
 	test.log(LogStatus.INFO, "Clicked PayBills button");
 	test.log(LogStatus.INFO, "Entered information in the new Payee form");
 	ap.payeeadd("enercare", "189 east avenue", "99999999", "Gas company Bill");
